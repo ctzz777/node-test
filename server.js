c31f9aa4038febe67905dec5fef88ea84de68642
CTzz <ctzz@protonmail.com>
2017-08-21 10:36:41 +0800

    projects pages

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/about', (req, res) => {
 
 app.get('/projects', (req, res) => {
     res.render('projects.hbs', {
-        pageTitle: 'About Pages'
+        pageTitle: 'Projects Pages'
     });
 });
 
